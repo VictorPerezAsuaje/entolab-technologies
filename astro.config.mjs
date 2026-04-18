cd1bace5db0656c480d7f7b07b5b3404eb6cfa60
Victor Pérez Asuaje <v.perezasuaje@gmail.com>
2026-04-18 11:57:22 +0200

    feat: añadido site y base al astro config para que recoja los assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://victorperezasuaje.github.io/entolab-technologies",
+    base: "/entolab-technologies"
+});
